4bf12f2de5bb129c527ce54a951e4520d6ffc3f8
soldor37 <seventhpirate@gmail.com>
2020-02-21 10:13:20 +0300

    packages table wo edit

---
+++ frontend/src/components/Comparisone.vue
@@ -0,0 +1,146 @@
+<template>
+  <div id="comparisone">
+    <router-view></router-view>
+    <b-container class="mt-2">
+      <b-row>
+        <b-col cols="4">
+          <b-form-select v-model="selectedPack1" :options="packages" value-field="idpack" text-field="pack_name" class="mb-2"></b-form-select>
+          <b-form-select v-model="selectedPack2" :options="packages" value-field="idpack" text-field="pack_name" class="mb-2"></b-form-select>
+          <b-button size="lg" variant="outline-dark" @click="calc();">Calculate</b-button>
+        </b-col>
+        <b-col cols="8">
+          <apexchart type="bar" :options="chart1.chartOptions" :series="chart1.series" ></apexchart>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+
+<script>
+import axios from "axios";
+export default {
+  name: "comparisone",
+  // components: {
+  //   apexchart: VueApexCharts
+  // },
+  data() {
+    return {
+      selectedPack1: 1,
+      selectedPack2: 2,
+      chart1: {
+        series:[],
+        chartOptions: {
+          chart: {
+            type: "bar"            
+          },
+          //TODO: вывод названия упаковки
+          legend: {
+            show: true,
+            formatter: function(seriesName, opts) {
+              return seriesName
+            },
+            fontSize: '16px'
+          },
+          plotOptions: {
+            bar: {
+              dataLabels: {
+                position: 'center'
+              },
+              horizontal: false,
+              columnWidth: "95%",
+              // endingShape: "rounded"
+            },
+          },
+          dataLabels: {
+              enabled: true,
+              dropShadow: {
+                  enabled: true,
+                  left: 2,
+                  top: 2,
+                  opacity: 0.5
+              },
+              style: {
+                fontSize: '18px',
+                //colors: ['#333']
+              },
+              
+            },
+          stroke: {
+            show: true,
+            width: 4,
+            colors: ["transparent"]
+          },
+          xaxis: {
+            categories: [
+              "air, m^2",
+              "water, l",
+              "energy, MJ",
+              "oil consumption, l"
+            ]
+          },
+          yaxis: {
+            title: {
+              text: "Relative values of environmental characteristics",
+              style: {
+                fontSize: '15px',
+                //colors: ['#333']
+              },
+            }
+          },
+          fill: {
+            opacity: 1
+          },
+          tooltip: {
+            y: {
+              formatter: function(val) {
+                return Number(val).toFixed(2);
+              }
+            }
+          }
+        }
+      },
+      packages: []
+    };
+  },
+  created() {
+    this.getPackages();
+  },
+  methods: {
+    getPackages() {
+      var app = this;
+      var hostname = window.location.hostname;
+      axios
+        .get(`http://${hostname}:3000/posts`)
+        .then(response => {
+          console.log(response);
+          app.packages = response.data;
+        })
+        .catch(error => {
+          alert(error + "\n Ошибка подключения к базе данных");
+          console.log("-----error-------");
+          console.log(error);
+        });
+    },
+    calc() {
+      var app = this;
+      var hostname = window.location.hostname;
+      let select = [app.selectedPack1, app.selectedPack2];
+      console.log(select);
+      axios
+        .post(`http://${hostname}:3000/posts/calc`,{
+          params: {
+            ID : select
+          }
+        })
+        .then(response => {
+          app.chart1.series = response.data;
+        })
+        .catch(error => {
+          console.log("-----error-------");
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,12 +5,35 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueRouter)
+//роутинг без загрузки страницы
+import VueRouter from 'vue-router'
 
 import VueApexCharts from 'vue-apexcharts'
-
+// импортируем AdminPanel компонент
+import AdminPanel from './components/AdminPanel'
+import Comparisone from './components/Comparisone'
 Vue.component('apexchart', VueApexCharts)
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+
+// инициализируем роуты
+const routes = [
+  { path: '/AdminPanel', component: AdminPanel },
+  { path: '/', component: Comparisone }
+  ]
+// Создаем экземпляр роутера и передайте опцию `routes`
+const router = new VueRouter({
+  routes,
+  mode: 'history'
 })
+// Создаем экземпляр vue
+new Vue({
+  // Определяем селектор для корневого компонента
+    el: '#app',
+    // передаем шаблон корневому компаненту
+    template: '<App/>',
+    // Объявляем компоненты, к которым может обращаться корневой компонент;
+    components: { App },
+    // Передаем в роутер экземпляр Vue
+    router
+  }).$mount('#app')
