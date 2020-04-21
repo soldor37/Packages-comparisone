ed327f12a68498fce4e11cbbbbbeb0eeb4235082
soldor37 <seventhpirate@gmail.com>
2020-04-21 08:53:20 +0300

    adminpanel w/o users

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -10,15 +10,15 @@ async function selectByLogin(login) {
     let conn = await connection.connect();
     return new Promise((resolve, reject) => {
         conn.promise().query(`SELECT * FROM users WHERE login = ?`, login)
-        .then(result =>{
-            ObjectUsers = [];
-            ObjectUsers = result[0];
-            resolve(ObjectUsers);
-            conn.release();
-          })
-          .catch(function(err) {
-            console.log(err.message);
-          });
+            .then(result => {
+                ObjectUsers = [];
+                ObjectUsers = result[0];
+                resolve(ObjectUsers);
+                conn.release();
+            })
+            .catch(function (err) {
+                console.log(err.message);
+            });
     });
 }
 //аутонтификация по логину и паролю
@@ -27,8 +27,9 @@ router.post('/login', (req, res) => {
         await selectByLogin(req.body.login)
         let passwordIsValid = bcrypt.compareSync(req.body.password, ObjectUsers[0].password);
         //console.log(bcrypt.hashSync(req.body.password,8))
-        if (!passwordIsValid)return res.status(401).send({ auth: false, token: null });
-        let token = jwt.sign({ id: ObjectUsers[0].id }, config.secret, { expiresIn: 86400 // expires in 24 hours
+        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+        let token = jwt.sign({ id: ObjectUsers[0].id }, config.secret, {
+            expiresIn: 86400 // expires in 24 hours
         });
         res.status(200).send({ auth: true, token: token, user: ObjectUsers[0] });
     })();
@@ -42,20 +43,21 @@ router.post('/login', (req, res) => {
 //         })
 // }
 //регистрация новых пользователей
-router.post('/register', function(req, res) {
+router.post('/register', function (req, res) {
     getConnection().Db_methods.insert([
         req.body.name,
         bcrypt.hashSync(req.body.password, 8)
     ],
-    function (err) {
-        if (err) return res.status(500).send("There was a problem registering the user.")
-        selectByName(req.body.name, (err,user) => {
-            if (err) return res.status(500).send("There was a problem getting user")
-            let token = jwt.sign({ id: user.id }, config.secret, {expiresIn: 86400 // expires in 24 hours
+        function (err) {
+            if (err) return res.status(500).send("There was a problem registering the user.")
+            selectByName(req.body.name, (err, user) => {
+                if (err) return res.status(500).send("There was a problem getting user")
+                let token = jwt.sign({ id: user.id }, config.secret, {
+                    expiresIn: 86400 // expires in 24 hours
+                });
+                res.status(200).send({ auth: true, token: token, user: user });
             });
-            res.status(200).send({ auth: true, token: token, user: user });
-        }); 
-    }); 
+        });
 });
 router.get('/users', function (req, res) {
     let sql = "SELECT * FROM users WHERE login = 'admin'";
@@ -84,50 +86,47 @@ router.get('/ecol_dict', function (req, res) {
 });
 
 router.post('/insert', function (req, res) {
-    console.log(req.body)
-    // let sql1 = `INSERT INTO packaging(pack_name) VALUES('${req.body.name}'); LAST_INSERT_ID();`
-    // let materials = req.body.materials;
-    // return new Promise(async (resolve, reject) => {
-    //     await connection.insert(sql1); //вносим изменения в таблицу названий упаковок
-    //     return new Promise(async (resolve, reject) => {
-    //         let conn = await connection.connect();
-    //         materials.forEach(function(mat){
-    //             conn.promise().query(`UPDATE material_weight SET material_weight = '${mat.mass}' WHERE fk_id_pack = '${req.body.id}' and fk_id_material = '${mat.idmaterials}';`)
-    //             .then(result =>{
-    //                 conn.release();
-    //             })
-    //             .catch(function(err) {
-    //                 console.log(err.message);
-    //         });
-    //     })
+    return new Promise(async (resolve, reject) => {
+        let lastID = 0;
+        let conn = await connection.connect();
+        conn.promise().query(`INSERT INTO packaging(pack_name) VALUES('${req.body.name}')`,
+            function (err, result) {
+                if (err) throw err;
+                lastID = result.insertId;
+                //console.log(lastID)
+                req.body.materials.forEach(function (mat) {
+                    connection.find(`INSERT INTO material_weight(fk_id_pack, fk_id_material, material_weight) VALUES('${lastID}', '${mat.id}','${mat.value}');`)
+                })
+            })
+            .then(result => {
+                conn.release();
+            })
+            .catch(function (err) {
+                console.log(err.message);
+            });
+        res.send('Data insert received');
+    });
 
-        
-    //     });
-    //     res.send('Data insert received');
-    // });
-    
 });
 //удаление упаковки вместе с принадлежащими ей весовыми значения материалов
 router.post('/delete', function (req, res) {
-    let sql = `DELETE FROM packaging WHERE idpack = '${req.body.id}';`
     let materials = req.body.materials;
+    let sql = `DELETE FROM packaging WHERE idpack = '${req.body.id}';`;
     return new Promise(async (resolve, reject) => {
-        await connection.del(sql); //вносим изменения в таблицу названий упаковок
-        return new Promise(async (resolve, reject) => {
-            let conn = await connection.connect();
-            materials.forEach(function(mat){
-                conn.promise().query(`DELETE FROM material_weight WHERE fk_id_pack = '${req.body.id}';`)
-                .then(result =>{
+        await connection.del(sql);
+        let conn = await connection.connect();
+        materials.forEach(function (mat) {
+            conn.promise().query(`DELETE FROM material_weight WHERE fk_id_pack = '${req.body.id}';`)
+                .then(result => {
                     conn.release();
                 })
-                .catch(function(err) {
+                .catch(function (err) {
                     console.log(err.message);
                 });
-            })
-        res.send('Data delete received')
+            
         });
+        res.send('Data delete received');
     });
-    
 });
 
 router.post('/edit', function (req, res) {
@@ -139,70 +138,87 @@ router.post('/edit', function (req, res) {
         await connection.update(sql1); //вносим изменения в таблицу названий упаковок
         return new Promise(async (resolve, reject) => {
             let conn = await connection.connect();
-            materials.forEach(function(mat){
+            materials.forEach(function (mat) {
                 conn.promise().query(`UPDATE material_weight SET material_weight = '${mat.mass}' WHERE fk_id_pack = '${req.body.id}' and fk_id_material = '${mat.idmaterials}';`)
-                .then(result =>{
-                    ecolkoeff.forEach(function(eco){
-                        conn.promise().query(`UPDATE ecol_criteria SET value = '${eco.value}' WHERE idecol_criteria = '${eco.idecol_criteria}';`)
+                    .then(result => {
+                        ecolkoeff.forEach(function (eco) {
+                            conn.promise().query(`UPDATE ecol_criteria SET value = '${eco.value}' WHERE idecol_criteria = '${eco.idecol_criteria}';`)
+                        });
+                    })
+                    .then(result => {
+                        conn.release();
+                    })
+                    .catch(function (err) {
+                        console.log(err.message);
                     });
-                })
-                .then(result =>{
-                    conn.release();
-                })
-                .catch(function(err) {
-                    console.log(err.message);
-            });
-        })
+            })
 
-        res.send('Data edit received');
+            res.send('Data edit received');
         });
-        
+
     });
-    
+
 });
 // ---------------работа с материалами(панель редактирования материалов)----------------
 router.post('/insertMaterial', function (req, res) {
-    console.log(req.body)
+    //console.log(req.body)
     let ecol_dict = [
         {
-            'name' : 'air',
+            'name': 'air',
             'message': 'm^3/kg'
         },
         {
-            'name' : 'water',
+            'name': 'water',
             'message': 'l/kg'
         },
         {
-            'name' : 'energy',
+            'name': 'energy',
             'message': 'MJ/kg'
         },
         {
-            'name' : 'oil',
+            'name': 'oil',
             'message': 'l/kg'
         },
     ]
-    let sql = `INSERT INTO materials(material_name) VALUES('${req.body.name_material}') `;
-    let lastID = 0;
-    let superLastID = 0;
     return new Promise(async (resolve, reject) => {
-        await connection.find(sql);
-        lastID = await connection.find(`SELECT LAST_INSERT_ID();`);
-        console.log(lastID)
-        superLastID = lastID[0]['LAST_INSERT_ID()'];
-        ecol_dict.forEach(function(eco){
-            connection.find(`INSERT INTO ecol_charact(ecol_name, fk_id_material, ecol_value, ecol_measure) VALUES('${eco.name}', '${superLastID}','${req.body[eco.name]}','${eco.message}');`)
-        })
+        let lastID = 0;
+        let conn = await connection.connect();
+        conn.promise().query(`INSERT INTO materials(material_name) VALUES('${req.body.name_material}')`,
+            function (err, result) {
+                if (err) throw err;
+                lastID = result.insertId;
+                //console.log(lastID)
+                ecol_dict.forEach(function (eco) {
+                    //console.log(`INSERT INTO ecol_charact(ecol_name, fk_id_material, ecol_value, ecol_measure) VALUES('${eco.name}', '${lastID}','${req.body[eco.name]}','${eco.message}');`)
+                    connection.find(`INSERT INTO ecol_charact(ecol_name, fk_id_material, ecol_value, ecol_measure) VALUES('${eco.name}', '${lastID}','${req.body[eco.name]}','${eco.message}');`)
+                })
+            })
+            .then(result => {
+                conn.release();
+            })
+            .catch(function (err) {
+                console.log(err.message);
+            });
         res.send('Data insert received');
     });
 });
 //удаление упаковки вместе с принадлежащими ей весовыми значения материалов
 router.post('/deleteMaterial', function (req, res) {
+    //console.log(req.body)
     let sql = `DELETE FROM materials WHERE idmaterials = '${req.body.idmaterials}';`;
     return new Promise(async (resolve, reject) => {
-        await connection.update(sql);
+        await connection.del(sql);
+        let conn = await connection.connect();
+        conn.promise().query(`DELETE FROM ecol_charact WHERE fk_id_material = '${req.body.idmaterials}';`)
+            .then(result => {
+                conn.release();
+            })
+            .catch(function (err) {
+                console.log(err.message);
+            });
         res.send('Data delete received');
     });
-    
+
 });
 
 router.post('/editMaterial', function (req, res) {
@@ -210,19 +226,19 @@ router.post('/editMaterial', function (req, res) {
     return new Promise(async (resolve, reject) => {
         await connection.update(sql);
         let conn = await connection.connect();
-        req.body.ecolcharacts.forEach(function(eco){
+        req.body.ecolcharacts.forEach(function (eco) {
             conn.promise().query(`UPDATE ecol_charact SET ecol_value = '${eco.value}' WHERE idecol = '${eco.idecol}';`)
-            .then(result =>{
-            conn.release();
-            })
-            .catch(function(err) {
-                console.log(err.message);
-            });
+                .then(result => {
+                    conn.release();
+                })
+                .catch(function (err) {
+                    console.log(err.message);
+                });
         })
-        res.send('Data edit received');                           
+        res.send('Data edit received');
     });
-        
-}); 
+
+});
 // ---------------------Расчеты для сравнения--------------------------
 router.post('/calc', function (req, res) {
     let packid = req.body.params.ID;
@@ -230,7 +246,7 @@ router.post('/calc', function (req, res) {
         await calculation(packid);
         res.send(ObjectGraph);
     })();
-        
+
 });
 var ObjectEcos = {
     packs: [],
@@ -242,8 +258,8 @@ var ObjectGraph = [];
 
 async function calculation(packid) {
     try {
-       await calcFormula1(packid);
-       await calcFormula2(packid);      
+        await calcFormula1(packid);
+        await calcFormula2(packid);
     }
     catch (error) {
         console.log(error.message);
@@ -253,13 +269,11 @@ async function calculation(packid) {
 async function calcFormula2(packid) {
     ObjectEcos.calculated = [];
     ObjectGraph = [];
-    for (let variable in packid){
-        if(typeof  ObjectGraph[variable] == 'undefined') 
-        {
+    for (let variable in packid) {
+        if (typeof ObjectGraph[variable] == 'undefined') {
             ObjectGraph[variable] = {};
         }
-        if(typeof  ObjectGraph[variable].name == 'undefined') 
-        {
+        if (typeof ObjectGraph[variable].name == 'undefined') {
             ObjectGraph[variable].name = 0;
         }
         await getPacks(packid[variable]);
@@ -267,59 +281,55 @@ async function calcFormula2(packid) {
         await getWeightMaterial(packid[variable]);
         var end_value = 0;
         ObjectEcos.weightMaterial.forEach(function (key) {
-        var name = key.ecol_name;
-        var weight = key.material_weight;
-        var value = key.ecol_value;
-        end_value = value * weight;
-        
-        if(typeof  ObjectEcos.calculated[variable] == 'undefined') 
-        {
-            ObjectEcos.calculated[variable] = [];
-        }
-        if(typeof  ObjectEcos.calculated[variable][name] == 'undefined') 
-        {
-            ObjectEcos.calculated[variable][name] = 0;
-        }
-        ObjectEcos.calculated[variable][name] += Number(end_value);
-    })
+            var name = key.ecol_name;
+            var weight = key.material_weight;
+            var value = key.ecol_value;
+            end_value = value * weight;
+
+            if (typeof ObjectEcos.calculated[variable] == 'undefined') {
+                ObjectEcos.calculated[variable] = [];
+            }
+            if (typeof ObjectEcos.calculated[variable][name] == 'undefined') {
+                ObjectEcos.calculated[variable][name] = 0;
+            }
+            ObjectEcos.calculated[variable][name] += Number(end_value);
+        })
     }
-    for (key in ObjectEcos.calculated){
-        for (let name in ObjectEcos.calculated[key]){
+    for (key in ObjectEcos.calculated) {
+        for (let name in ObjectEcos.calculated[key]) {
             ObjectEcos.calculated[key][name] = Number(ObjectEcos.calculated[key][name]) / Number(ObjectEcos.comparativeWeight[name]);
-            if(typeof  ObjectGraph[key].data == 'undefined') 
-            {
+            if (typeof ObjectGraph[key].data == 'undefined') {
                 ObjectGraph[key].data = [];
             }
             ObjectGraph[key].data.push((Number(ObjectEcos.calculated[key][name])).toFixed(3));
         }
-        
+
     }
     console.log(ObjectGraph);
     //console.log(ObjectEcos.calculated);
     return Promise.resolve(ObjectGraph);
-    
+
 }
 //Считает сумму абсолютных значений экологических характеристик выбранных упаковок для значенателя второй формулы
- async function calcFormula1(packid) {
-            ObjectEcos.comparativeWeight = [];
-            for (let variable in packid){
-                //console.log("упаковка -",packid[variable]);
-              await getWeightMaterial(packid[variable]);
-                var end_value = 0;
-                ObjectEcos.weightMaterial.forEach(function (key) {
-                    var name = key.ecol_name;
-                    var weight = key.material_weight;
-                    var value = key.ecol_value;
-                    end_value = value * weight;
-                    //console.log("данные:",value,weight,end_value);
-                    if(typeof  ObjectEcos.comparativeWeight[name] == 'undefined') 
-                    {
-                        ObjectEcos.comparativeWeight[name] = 0;
-                    }
-                    ObjectEcos.comparativeWeight[name] += Number(end_value);
-                })
-                //console.log("Добавили в структуру такую вот фигню -",ObjectEcos.comparativeWeight);   
-            }    
+async function calcFormula1(packid) {
+    ObjectEcos.comparativeWeight = [];
+    for (let variable in packid) {
+        //console.log("упаковка -",packid[variable]);
+        await getWeightMaterial(packid[variable]);
+        var end_value = 0;
+        ObjectEcos.weightMaterial.forEach(function (key) {
+            var name = key.ecol_name;
+            var weight = key.material_weight;
+            var value = key.ecol_value;
+            end_value = value * weight;
+            //console.log("данные:",value,weight,end_value);
+            if (typeof ObjectEcos.comparativeWeight[name] == 'undefined') {
+                ObjectEcos.comparativeWeight[name] = 0;
+            }
+            ObjectEcos.comparativeWeight[name] += Number(end_value);
+        })
+        //console.log("Добавили в структуру такую вот фигню -",ObjectEcos.comparativeWeight);   
+    }
 }
 
 async function getWeightMaterial(idpack) {
@@ -336,18 +346,18 @@ async function getWeightMaterial(idpack) {
         ecol_charact.fk_id_material = material_weight.fk_id_material
         where
         material_weight.fk_id_pack = `+ idpack)
-        .then(result =>{
-            ObjectEcos.weightMaterial = result[0];
-          })
-          .then(result =>{
-            //console.log(ObjectEcos.weightMaterial);
-            resolve(ObjectEcos.weightMaterial);
-            conn.release();
-          })
-          .catch(function(err) {
-            console.log(err.message);
-            reject();
-          });
+            .then(result => {
+                ObjectEcos.weightMaterial = result[0];
+            })
+            .then(result => {
+                //console.log(ObjectEcos.weightMaterial);
+                resolve(ObjectEcos.weightMaterial);
+                conn.release();
+            })
+            .catch(function (err) {
+                console.log(err.message);
+                reject();
+            });
     });
 }
 
@@ -356,21 +366,21 @@ async function getPacks(idpack) {
     return new Promise((resolve, reject) => {
         //console.log("За инфой Пришла пачка -",idpack);
         conn.promise().query(`SELECT idpack, pack_name FROM packaging WHERE idpack =` + idpack)
-        .then(result =>{
-            ObjectEcos.packs = result[0];
-          })
-          .then(result =>{
-            console.log(ObjectEcos.packs);
-            resolve(ObjectEcos.packs);
-            conn.release();
-          })
-          .catch(function(err) {
-            console.log(err.message);
-            reject();
-          });
+            .then(result => {
+                ObjectEcos.packs = result[0];
+            })
+            .then(result => {
+                console.log(ObjectEcos.packs);
+                resolve(ObjectEcos.packs);
+                conn.release();
+            })
+            .catch(function (err) {
+                console.log(err.message);
+                reject();
+            });
     });
 }
- 
+
 //------------Парсинг таблиц из БД----------------
 //таблица названий материалов
 router.get('/DBmaterials', function (req, res) {
